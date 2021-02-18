ceddeece7a9ad5f3efaa8f49e3ca22d2b06569f8
Noah <const112@umn.edu>
2021-02-18 16:29:14 -0600

    Implement Todo Profile View
    Implemented todo profile view. Displays owner, category, status, and body. Removed tests temporarily and will re-implement at a later time.

--- client/src/app/todos/todo-profile/todo-profile.component.ts
+++ client/src/app/todos/todo-profile/todo-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Todo } from '../todo';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-profile',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoProfileComponent implements OnInit {
 
-  constructor() { }
+  todo: Todo;
+  id: string;
+
+  constructor(private route: ActivatedRoute, private todoService: TodoService) { }
 
   ngOnInit(): void {
+    // We subscribe to the parameter map here so we'll be notified whenever
+    // that changes (i.e., when the URL changes) so this component will update
+    // to display the newly requested user.
+    this.route.paramMap.subscribe((pmap) => {
+      this.id = pmap.get('id');
+      this.todoService.getTodoById(this.id).subscribe(todo => this.todo = todo);
+    });
   }
 
 }
